048f2eec1dcc39bcd9af804c5bff98573cd45188
Petru Valicov <petru.valicov@univ-amu.fr>
2019-01-25 16:20:48 +0100

    creation structure du projet

---
+++ src/main/java/fr/umontpellier/iut/exo1/GestionEtudiants.java
@@ -0,0 +1,9 @@
+package fr.umontpellier.iut.exo1;
+
+public class GestionEtudiants {
+
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+}
---
+++ src/main/java/fr/umontpellier/iut/exo2/GestionEtudiants.java
@@ -0,0 +1,9 @@
+package fr.umontpellier.iut.exo2;
+
+public class GestionEtudiants {
+
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+}
