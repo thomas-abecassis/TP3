82baa225a9eb3b20e3c1d31ebbe9a65045d19ade
Victor Poupet <victor.poupet@gmail.com>
2019-02-02 18:35:37 +0100

    - Remis la méthode main dans les 3 versions de GestionEtudiants

--- src/main/java/fr/umontpellier/iut/exo2/GestionEtudiants.java
+++ src/main/java/fr/umontpellier/iut/exo2/GestionEtudiants.java
@@ -1,5 +1,7 @@
 package fr.umontpellier.iut.exo2;
 
 public class GestionEtudiants {
-
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented");
+    }
 }
--- src/main/java/fr/umontpellier/iut/exo3/GestionEtudiants.java
+++ src/main/java/fr/umontpellier/iut/exo3/GestionEtudiants.java
@@ -1,5 +1,7 @@
 package fr.umontpellier.iut.exo3;
 
 public class GestionEtudiants {
-
+    public static void main(String[] args) {
+        throw new RuntimeException("Not yet implemented");
+    }
 }
